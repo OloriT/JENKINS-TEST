6e8d8f73c6e67270b50a0d468f5fdc2fd2ca90f6
OloriT <oloritaiwo@gmail.com>
2025-05-17 23:30:04 +0100

    Made change to dev branch yml

---
+++ demo.py
@@ -0,0 +1,11 @@
+name: Dev env workflow
+
+on: [push]
+
+jobs:
+  Test:
+    runs-on: ubuntu-latest
+    steps: 
+      - uses: actions/checkout@v3
+      - run: echo "Tell it was automatically triggered by a ${{ github.event_name }} event."
+      - run: python demo.py
