0025485182ba25f999c24ab7fdad22f58bffdd86
OloriT <oloritaiwo@gmail.com>
2025-05-17 23:45:49 +0100

    Made corrections

--- demo.py
+++ demo.py
@@ -1,14 +1,1 @@
-name: Dev env workflow
-
-on:
-  push:
-    branches:
-      - dev
-
-jobs:
-  Test:
-    runs-on: ubuntu-latest
-    steps: 
-      - uses: actions/checkout@v3
-      - run: echo "Tell it was automatically triggered by a ${{ github.event_name }} event."
-      - run: python demo.py
+print("I am a Software Tester")
